bc16b12434ffbb520ace57b4850d4d07eae5b048
nabar001 <ryan.nabat@disney.com>
2015-11-12 23:25:24 -0800

    Retrieve data from /r/all

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  var request = require('request')
+  var reddit_base_url = 'https://reddit.com'
+  request(reddit_base_url+'/r/all.json', function(err, response, body){
+    body = JSON.parse(body)
+    res.render('index', { title: 'Express', posts: body.data.children });
+  })
 });
 
 module.exports = router;
