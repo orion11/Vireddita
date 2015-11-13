dfdecbcf51beb13121e16231a03c3e23c39379ba
nabar001 <ryan.nabat@disney.com>
2015-11-13 02:38:18 -0800

    Render favorites list

---
+++ public/javascripts/favorites.js
@@ -0,0 +1,32 @@
+function renderFavorites() {
+  var favorites = Favorites.get()
+  var dom = {
+    container: $(".favorites"),
+    post: $("<li>").addClass("post"),
+    thumbnail: $("<img>").addClass("thumbnail"),
+    title: $("<a>").addClass("title"),
+    icon: $("<span>").addClass("favorite").append($("<i>").addClass("fa fa-circle")),
+    clone: function() {
+      return {
+        post: this.post.clone(),
+        thumbnail: this.thumbnail.clone(),
+        title: this.title.clone(),
+        icon: this.icon.clone()
+      }
+    }
+  }
+
+  favorites.data.forEach(function(fav){
+    var post = dom.clone()
+    post.post.attr("id", fav.id)
+    post.title.attr("href", fav.url).text(fav.title)
+    if(fav.thumbnail) {
+      post.thumbnail.attr("src", fav.thumbnail)
+      post.post.append(post.thumbnail)
+    }
+    post.post.append(post.title, post.icon)
+    dom.container.append(post)
+  })
+}
+
+renderFavorites()
